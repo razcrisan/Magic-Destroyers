743e2d59d638226f3b90069eee325c73a2a176cc
Razvan Crisan <rcrisan13@gmail.com>
2018-03-16 15:09:50 -0400

    added knight

--- Characters/Melee/Knight.cs
+++ Characters/Melee/Knight.cs
@@ -1,7 +1,40 @@
+using MagicDestroyers.Equipment.Armors.Heavy;
+using MagicDestroyers.Equipment.Weapons.Blunt;
+using System;
+
 namespace MagicDestroyers.Characters.Melee
 {
     public class Knight
     {
         
+        private int abilityPoints;
+        private int healthPoints;
+        private int level;
+
+        private string faction;
+        private string name;
+
+        private Chainlink bodyArmor;
+        private Hammer weapon;
+        
+        public Knight()
+        {
+
+        }
+
+        public void HolyBlow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurifySoul()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RighteousWings()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
