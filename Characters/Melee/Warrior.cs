9924d11f5e657f1f8eda02a962dc197db698bd1a
Razvan Crisan <rcrisan13@gmail.com>
2018-03-20 12:01:56 -0400

    added value restrictions

--- Characters/Melee/Warrior.cs
+++ Characters/Melee/Warrior.cs
@@ -12,6 +12,74 @@ namespace MagicDestroyers.Characters.Melee
         private string faction;
         private string name;
         private Chainlink bodyArmor;
-    
+        private Axe weapon;
+        public int AbilityPoints { 
+            get{
+                return abilityPoints;
+            }
+            set{
+                if(value >= 0 && value <=10)
+                {
+                abilityPoints = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Innapropraite Value, should be >= to 0 and <= 10.");
+                }
+            }
+        }
+          public int HealthPoints { 
+            get{
+                return abilityPoints;
+            }
+            set{
+                if(value >= 0 && value <=10)
+                {
+                healthPoints = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Innapropraite Value, should be higher or equal to 0 and <= 10.");
+                }
+            }
+        }
+          public int Level { 
+            get{
+                return abilityPoints;
+            }
+            set{
+                if(value >= 0 && value <=10)
+                {
+                level = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Innapropriate Value, should be higher or equal to 0 and <= 10.");
+                }
+            }
+        }
+
+        
+
+        public Warrior()
+        {
+
+        }
+
+        public void Strike()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SkinHarden()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
