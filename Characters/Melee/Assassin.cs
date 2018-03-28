8bdd41d87530c97e0fe00860b2085dffd3107673
Razvan Crisan <rcrisan13@gmail.com>
2018-03-28 10:41:22 -0400

    added assassin class

--- Characters/Melee/Assassin.cs
+++ Characters/Melee/Assassin.cs
@@ -17,8 +17,90 @@ namespace MagicDestroyers.Characters.Melee
         private Sword weapon;
 
         public Assassin()
+            : this("Rogue", 120)
         {
+        }
+        public Assassin(string name, int healthPoints)
+            : this(name, 120, 1, 100)
+        {
+        }
+        public Assassin(string name, int healthPoints, int level, int abilityPoints)
+        {
+            this.name = name;
+            this.healthPoints = healthPoints;
+            this.level = 1;
+            this.abilityPoints = 100;
+            this.bodyArmor = new LightLeatherVest();
+            this.weapon = new Sword();
+        }
+        public int AbilityPoints { 
+            get
+            {
+                return this.abilityPoints;
+            }
+            set
+            {
+                if(value >= 0 && value <=10)
+                {
+                this.abilityPoints = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Innapropraite Value, should be >= to 0 and <= 10.");
+                }
+            }
+        }
 
+        public int HealthPoints {
+            get
+            {
+                return this.healthPoints;
+            }
+            set
+            {
+                if(value >= 0 && value <=10)
+                {
+                    this.abilityPoints = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Wrong Value, should be >= 0  and <= 10");
+                }
+            }
+        }
+        public int Level { 
+            get
+            {
+                return this.level;
+            }
+            set
+            {
+                if(value >= 0 && value <=10)
+                {
+                this.level = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Innapropriate Value, should be higher or equal to 0 and <= 10.");
+                }
+            }
+        }
+            public string Name { 
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if(value.Length >= 3 && value.Length <=12)
+                {
+                this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Innapropriate Value, should be higher or equal to 0 and <= 10.");
+                }
+            }
         }
 
         public void Raze()
